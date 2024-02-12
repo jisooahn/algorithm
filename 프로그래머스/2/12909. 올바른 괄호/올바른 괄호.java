0dce340e164535fd98c2f3187b991051cabc6309
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-02-12 13:36:58 +0900

    [level 2] Title: 올바른 괄호, Time: 4.86 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,10 +1,16 @@
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
-        return answer;
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                count++;
+            } else {
+                count--;
+            }
+            if (count < 0) {
+                break;
+            }
+        }
+        return count == 0 ? true: false;
     }
 }
