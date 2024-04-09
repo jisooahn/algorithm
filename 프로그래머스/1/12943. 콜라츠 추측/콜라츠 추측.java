5f1c07f75e9b35549fc556285827c967723da3d6
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-04-09 16:23:19 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -3,12 +3,9 @@ class Solution {
         int answer = 0;
         long n = num;
         while (n != 1) {
-            if (answer >= 500) {
-                return -1;
-            }
             n = n % 2 == 0 ? n / 2 : n * 3 + 1;
             answer++;
         }
-        return answer;
+        return answer >= 500 ? -1 : answer;
     }
 }
