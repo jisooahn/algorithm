54c476bdc2b9a9a8ddac8c8a3dfeda21058d34f3
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-04-10 19:25:52 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -2,11 +2,7 @@ class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[2];
         for (int num : num_list) {
-            if (num % 2 == 0) {
-                answer[0]++;
-            } else {
-                answer[1]++;
-            }
+            answer[num % 2]++;
         }
         return answer;
     }
