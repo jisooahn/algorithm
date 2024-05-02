40a137253c6c151dde91e64c22bbb802eeee74a8
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-05-02 16:30:19 +0900

    [level 1] Title: 시저 암호, Time: 2.02 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,16 +1,14 @@
 class Solution {
     public String solution(String s, int n) {
         StringBuilder sb = new StringBuilder();
-        final String lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
-        final String upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         for (char c : s.toCharArray()) {
             if (c == ' ') {
-                sb.append(' ');
-                continue;
-            } 
-            String alphabet = Character.isLowerCase(c) ? lowerAlphabet : upperAlphabet;
-            int idx = alphabet.indexOf(c);
-            sb.append(alphabet.charAt((idx + n) % 26));
+                sb.append(c);
+            } else if (Character.isLowerCase(c)) {
+                sb.append((char) ('a' + (c - 'a' + n) % 26));
+            } else {
+                sb.append((char) ('A' + (c - 'A' + n) % 26));
+            }
         }
         return sb.toString();
     }
