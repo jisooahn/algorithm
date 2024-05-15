ce5ae2c25711d732aba926d54d036d57755e451f
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-05-16 08:59:47 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.35 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -8,10 +8,8 @@ class Solution {
 
         if (dice[0] == dice[3]) {
             answer = 1111 * dice[0];
-        } else if (dice[0] == dice[2]) {
-            answer = (int) Math.pow(10 * dice[0] + dice[3], 2);
-        } else if (dice[1] == dice[3]) {
-            answer = (int) Math.pow(10 * dice[1] + dice[0], 2);
+        } else if (dice[0] == dice[2] || dice[1] == dice[3]) {
+            answer = (int) Math.pow(10 * dice[1] + (dice[0] + dice[3] - dice[1]), 2);
         } else if (dice[0] == dice[1] && dice[2] == dice[3]) {
             answer = (dice[0] + dice[2]) * Math.abs(dice[0] - dice[2]);
         } else if (dice[0] == dice[1]) {
