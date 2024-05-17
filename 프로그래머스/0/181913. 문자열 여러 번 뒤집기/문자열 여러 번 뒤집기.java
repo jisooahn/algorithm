be61ad894f37d5f0c161c327d1e8fde95e3dbc29
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-05-17 11:21:18 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 3.22 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,12 +1,13 @@
 class Solution {
     public String solution(String my_string, int[][] queries) {
+        StringBuilder sb = new StringBuilder(my_string);
         for (int[] query : queries) {
             int s = query[0];
             int e = query[1];
-            StringBuilder sb = new StringBuilder(my_string.substring(s, e + 1));
-            String reverse = sb.reverse().toString();
-            my_string = my_string.substring(0, s) + reverse + my_string.substring(e + 1);
+            StringBuilder tmp = new StringBuilder(sb.substring(s, e + 1));
+            String reverse = tmp.reverse().toString();
+            sb.replace(s, e + 1, reverse);
         }
-        return my_string;
+        return sb.toString();
     }
 }
