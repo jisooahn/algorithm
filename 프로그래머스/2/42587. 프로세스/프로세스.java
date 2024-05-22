9b0a6ef4d674cca8f2d4e3bf3118be19ffd86464
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-05-22 20:55:11 +0900

    [level 2] Title: 프로세스, Time: 0.67 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < priorities.length; i++) {
+            queue.offer(new int[]{priorities[i], i});
+        }
+
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            boolean isHighestPriority = true;
+            int[] currentProcess = queue.poll();
+            for (int[] queuedProcess : queue) {
+                if (currentProcess[0] < queuedProcess[0]) {
+                    isHighestPriority = false;
+                    queue.offer(currentProcess);
+                    break;
+                }
+            }
+
+            if (isHighestPriority) {
+                count++;
+                if (currentProcess[1] == location) {
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
