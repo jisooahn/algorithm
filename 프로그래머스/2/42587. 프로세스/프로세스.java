5fcc9c0f9b5a73f7e3ddc99b479db302f091ec77
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-05-23 23:30:41 +0900

    [level 2] Title: 프로세스, Time: 0.50 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,28 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<int[]> queue = new ArrayDeque<>();
-        for (int i = 0; i < priorities.length; i++) {
-            queue.offer(new int[]{priorities[i], i});
+        Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int priority : priorities) {
+            queue.offer(priority);
         }
 
         int count = 0;
 
         while (!queue.isEmpty()) {
-            boolean isHighestPriority = true;
-            int[] currentProcess = queue.poll();
-            for (int[] queuedProcess : queue) {
-                if (currentProcess[0] < queuedProcess[0]) {
-                    isHighestPriority = false;
-                    queue.offer(currentProcess);
-                    break;
-                }
-            }
-
-            if (isHighestPriority) {
-                count++;
-                if (currentProcess[1] == location) {
-                    break;
+            for (int i = 0; i < priorities.length; i++) {
+                if (queue.peek() == priorities[i]) {
+                    queue.poll();
+                    count++;
+                    
+                    if (location == i) {
+                        return count;
+                    }
                 }
             }
         }
