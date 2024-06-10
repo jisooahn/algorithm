936255045b1a49f710ac6904306f7c3dc0346034
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-06-10 14:16:48 +0900

    [level 0] Title: 5명씩, Time: 0.04 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -6,11 +6,6 @@ class Solution {
         for (int i = 0; i < names.length; i += 5) {
             list.add(names[i]);
         }
-        
-        String[] answer = new String[list.size()];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i);
-        }
-        return answer;
+        return list.toArray(new String[0]);
     }
 }
