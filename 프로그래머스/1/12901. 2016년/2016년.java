8b0e7c76e6b4397699cd06e081d13d7d543d307e
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-06-14 17:48:59 +0900

    [level 1] Title: 2016년, Time: 1.01 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,13 +1,7 @@
-class Solution {
-    public static int[] daysInMonth = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    public static String[] daysOfWeek = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+import java.time.*;
 
+class Solution {
     public String solution(int a, int b) {
-        int days = 0;
-        for (int i = 1; i < a; i++) {
-            days += daysInMonth[i];
-        }
-        days += b;
-        return daysOfWeek[days % 7];
+        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0, 3);
     }
 }
