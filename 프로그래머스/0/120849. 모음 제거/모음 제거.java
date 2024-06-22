2c4d4420652b69514b5543b6f7e25e510cdc19ea
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-06-23 00:21:45 +0900

    [level 0] Title: 모음 제거, Time: 0.10 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,9 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        String[] aeiou = {"a", "e", "i", "o", "u"};
-        for (String a : aeiou) {
-            my_string = my_string.replace(a, "");
-        }
-        return my_string;
+        return my_string.replaceAll("[aeiou]", "");
     }
 }
