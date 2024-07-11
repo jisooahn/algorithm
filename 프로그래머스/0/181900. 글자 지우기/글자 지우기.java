204e00742f42c89ccac6a78f73ba1f99bf9cfc98
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-07-11 10:20:29 +0900

    [level 0] Title: 글자 지우기, Time: 0.27 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,15 +1,13 @@
 class Solution {
     public String solution(String my_string, int[] indices) {
-        boolean[] isDeleted = new boolean[my_string.length()];
+        String[] split = my_string.split("");
         for (int indicy : indices) {
-            isDeleted[indicy] = true;
+            split[indicy] = "";
         }
         
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < my_string.length(); i++) {
-            if (!isDeleted[i]) {
-                sb.append(my_string.charAt(i));
-            }
+        for (String s : split) {
+            sb.append(s);
         }
         
         return sb.toString();
