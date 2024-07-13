be5632f36b3b9b4ab58b3ec32c91e7d1acd025a2
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-07-13 14:06:11 +0900

    [level 0] Title: 리스트 자르기, Time: 0.05 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -7,22 +7,27 @@ class Solution {
         int b = slicer[1];
         int c = slicer[2];
         
-        if (n == 1) {
-            for (int i = 0; i <= b; i++) {
-                list.add(num_list[i]);
-            }
-        } else if (n == 2) {
-            for (int i = a; i < num_list.length; i++) {
-                list.add(num_list[i]);
-            }
-        } else if (n == 3) {
-            for (int i = a; i <= b; i++) {
-                list.add(num_list[i]);
-            }
-        } else if (n == 4) {
-            for (int i = a; i <= b; i += c) {
-                list.add(num_list[i]);
-            }
+        switch (n) {
+            case 1:
+                for (int i = 0; i <= b; i++) {
+                    list.add(num_list[i]);
+                }
+                break;
+            case 2:
+                for (int i = a; i < num_list.length; i++) {
+                    list.add(num_list[i]);
+                }
+                break;
+            case 3:
+                for (int i = a; i <= b; i++) {
+                    list.add(num_list[i]);
+                }
+                break;
+            case 4:
+                for (int i = a; i <= b; i += c) {
+                    list.add(num_list[i]);
+                }
+                break;
         }
         
         int[] answer = new int[list.size()];
