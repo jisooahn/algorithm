fe4b008d7b7e4fdcb70d53f7c3c41953be702347
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-07-13 14:08:22 +0900

    [level 0] Title: 리스트 자르기, Time: 0.07 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -8,26 +8,26 @@ class Solution {
         int c = slicer[2];
         
         switch (n) {
-            case 1:
+            case 1 -> {
                 for (int i = 0; i <= b; i++) {
                     list.add(num_list[i]);
                 }
-                break;
-            case 2:
+            }
+            case 2 -> {
                 for (int i = a; i < num_list.length; i++) {
                     list.add(num_list[i]);
                 }
-                break;
-            case 3:
+            }
+            case 3 -> {
                 for (int i = a; i <= b; i++) {
                     list.add(num_list[i]);
                 }
-                break;
-            case 4:
+            }
+            case 4 -> {
                 for (int i = a; i <= b; i += c) {
                     list.add(num_list[i]);
                 }
-                break;
+            }
         }
         
         int[] answer = new int[list.size()];
