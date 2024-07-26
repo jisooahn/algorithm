1f616bf049f139d8b7749a0b1d620309edf8ff6f
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-07-26 23:22:17 +0900

    [level 0] Title: 1로 만들기, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -3,11 +3,7 @@ class Solution {
         int answer = 0;
         for (int num : num_list) {
             while (num != 1) {
-                if (num % 2 == 0) {
-                    num /= 2;
-                } else {
-                    num = (num - 1) / 2;
-                }
+                num /= 2;
                 answer++;
             }
         }
