8b102e7092dc750a1416dc93bc2c1335712685af
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-08-02 13:54:45 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.62 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -3,11 +3,6 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] num_list) {
         Arrays.sort(num_list);
-        int[] answer = new int[num_list.length - 5];
-        int idx = 0;
-        for (int i = 5; i < num_list.length; i++) {
-            answer[idx++] = num_list[i];
-        }
-        return answer;
+        return Arrays.copyOfRange(num_list, 5, num_list.length);
     }
 }
