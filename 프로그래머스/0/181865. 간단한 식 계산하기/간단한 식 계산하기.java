dbdb7f6113df6246c36323dc8e3b22be1ea6f8b8
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-08-15 12:27:37 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.12 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -1,14 +1,14 @@
 class Solution {
     public int solution(String binomial) {
-        int answer = 0;
         String[] split = binomial.split(" ");
         int a = Integer.valueOf(split[0]);
         int b = Integer.valueOf(split[2]);
-        switch (split[1]) {
+        int answer = switch (split[1]) {
             case "+" -> answer = a + b;
             case "-" -> answer = a - b;
             case "*" -> answer = a * b;
-        }
+            default -> throw new IllegalArgumentException("Invalid Operator");
+        };
         return answer;
     }
 }
