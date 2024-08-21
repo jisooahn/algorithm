537973cca2f0acc3ed80c727886a84c40d900a4b
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-08-21 19:05:29 +0900

    [level 0] Title: 문자열 묶기, Time: 10.80 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -1,14 +1,10 @@
 class Solution {
     public int solution(String[] strArr) {
         int[] lenCount = new int[strArr.length + 1];
+        int answer = 0;
         for (String str : strArr) {
             lenCount[str.length()]++;
-        }
-        int answer = lenCount[0];
-        for (int count : lenCount) {
-            if (answer < count) {
-                answer = count;
-            }
+            answer = Math.max(answer, lenCount[str.length()]);
         }
         return answer;
     }
