7cc1bf4c5394fdd8d66f597867367bb8884fa27c
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-09-19 22:11:03 +0900

    [level 0] Title: 모스부호 (1), Time: 0.28 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -1,13 +1,15 @@
+import java.util.*;
+
 class Solution {
     public String solution(String letter) {
         StringBuilder sb = new StringBuilder();
         String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        Map<String, Character> map = new HashMap<>();
+        for (int i = 0; i < morse.length; i++) {
+            map.put(morse[i], (char) ('a' + i));
+        }
         for (String s : letter.split(" ")) {
-            for (int i = 0; i < morse.length; i++) {
-                if (s.equals(morse[i])) {
-                    sb.append(Character.toString('a' + i));
-                }
-            }
+            sb.append(map.get(s));
         }
         return sb.toString();
     }
