30ed8a27c5fe1087757c8abd0fc8626091362d20
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-10-04 08:08:06 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0;
         while (order != 0) {
             int digit = order % 10;
-            if (digit == 3 || digit == 6 || digit == 9) {
+            if (digit % 3 == 0 && digit != 0) {
                 answer++;
             }
             order /= 10;
