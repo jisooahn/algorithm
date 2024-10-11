1b6bcd259538474832f1f1c41a46ad2a86b1c80c
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-10-11 22:47:46 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -2,11 +2,11 @@ class Solution {
     public int solution(int[] array) {
         int answer = 0;
         for (int a : array) {
-            String str = Integer.toString(a);
-            for (char c : str.toCharArray()) {
-                if (c == '7') {
+            while (a > 0) {
+                if (a % 10 == 7) {
                     answer++;
                 }
+                a /= 10;
             }
         }
         return answer;
