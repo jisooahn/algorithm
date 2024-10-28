2155654810886eaf53fe1f91677c1b389991a184
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-10-29 00:02:40 +0900

    [Bronze V] Title: 합, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -5,10 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += i;
-        }
+        int sum = (1 + n) * n / 2;
         System.out.println(sum);
     }
 
