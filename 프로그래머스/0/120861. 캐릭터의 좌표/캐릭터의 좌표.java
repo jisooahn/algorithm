986e1fb246ed00713e202f5f4e092bad14f6da6c
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-10-30 21:29:41 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.05 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] now = {0, 0};
+        int maxX = board[0] / 2;
+        int maxY = board[1] / 2;
+        
+        for (String key : keyinput) {
+            switch (key) {
+                case "right": now[0] = Math.min(now[0] + 1, maxX);
+                    break;
+                case "left": now[0] = Math.max(now[0] - 1, -maxX);
+                    break;
+                case "up": now[1] = Math.min(now[1] + 1, maxY);
+                    break;
+                case "down": now[1] = Math.max(now[1] - 1, -maxY);
+                    break;
+            }
+        }
+        return now;
+    }
+}
