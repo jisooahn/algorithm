a63a88b7dad65be6a52604f2247ae590971e84cd
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-11-01 23:20:19 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,38 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
+    private static int[][] DIRECTIONS = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    
     public int solution(int[][] board) {
-        int rows = board.length;
-        int cols = board[0].length;
-        List<int[]> mines = new ArrayList<>();
-        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
-        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+        int n = board.length;
+        boolean[][] dangerZone = new boolean[n][n];
         
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (board[i][j] == 1) {
-                    mines.add(new int[]{i, j});
-                }
-            }
-        }
-        
-        for (int[] mine : mines) {
-            int x = mine[0];
-            int y = mine[1];
-            for (int k = 0; k < 8; k++) {
-                int nextX = x + dx[k];
-                int nextY = y + dy[k];
-                if (0 <= nextX && nextX < rows && 0 <= nextY && nextY < cols && board[nextX][nextY] == 0) {
-                    board[nextX][nextY] = 1;
+                    dangerZone[i][j] = true;
+                    for (int k = 0; k < DIRECTIONS.length; k++) {
+                        int nearX = i + DIRECTIONS[k][0];
+                        int nearY = j + DIRECTIONS[k][1];
+                        if (!(0 <= nearX && nearX < n && 0 <= nearY && nearY < n)) {
+                            continue;
+                        }
+                        dangerZone[nearX][nearY] = true;
+                    }
                 }
             }
         }
         
         int answer = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (board[i][j] == 0) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!dangerZone[i][j]) {
                     answer++;
                 }
             }
