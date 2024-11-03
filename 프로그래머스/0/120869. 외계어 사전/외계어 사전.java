f96a2fc0fc7caaa9e4d8bc38d3a1fda0c9bf3188
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-11-03 18:07:32 +0900

    [level 0] Title: 외계어 사전, Time: 0.15 ms, Memory: 69.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        for (String word : dic) {
+            int count = 0;
+            for (String s : spell) {
+                if (word.contains(s)) {
+                    count++;
+                }
+            }
+            if (count == spell.length) {
+                return 1;
+            }
+        }
+        return 2;
+    }
+}
