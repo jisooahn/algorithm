edd6d2fb4fec678a601c4e703bae0d6d4c8f582e
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-11-06 23:11:56 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.16 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -1,18 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int solution(int[][] lines) {
-        int answer = 0;
-        int[] points = new int[201];
-        for (int i = 0; i < lines.length; i++) {
-            for (int j = lines[i][0]; j < lines[i][1]; j++) {
-                points[j + 100]++;
-            }
-        }
+        Set<Integer> points = new HashSet<>();
         
-        for (int point : points) {
-            if (point >= 2) {
-                answer++;
+        for (int i = 0; i < lines.length - 1; i++) {
+            for (int j = i + 1; j < lines.length; j++) {
+                int start = Math.max(lines[i][0], lines[j][0]);
+                int end = Math.min(lines[i][1], lines[j][1]);
+                if (end > start) {
+                    for (int k = start; k < end; k++) {
+                        points.add(k);
+                    }
+                }
             }
         }
-        return answer;
+        return points.size();
     }
 }
