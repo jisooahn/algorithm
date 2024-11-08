5a2270b092a76f014f576c12dc38737b53ae317d
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-11-08 17:15:43 +0900

    [level 0] Title: 옹알이 (1), Time: 0.60 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,13 +1,20 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] words = {"aya", "ye", "woo", "ma"};
-        for (int i = 0; i < babbling.length; i++) {
-            for (String word : words) {
-                babbling[i] = babbling[i].replace(word, " ");
+        Set<String> set = new HashSet<>(List.of("aya", "ye", "woo", "ma"));
+        for (String b : babbling) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : b.toCharArray()) {
+                sb.append(c);
+                if (set.contains(sb.toString())) {
+                    sb.setLength(0);
+                }
             }
-            
-            if (babbling[i].trim().isEmpty()) {
+            if (sb.length() == 0) {
                 answer++;
             }
         }
