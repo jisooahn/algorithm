1229c892fcbad0778d309411f52274620bca1f17
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-11-09 18:00:27 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,16 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
         String id = id_pw[0];
         String pw = id_pw[1];
+        
+        Map<String, String> map = new HashMap<>();
         for (String[] dbIdPw : db) {
-            String dbId = dbIdPw[0];
-            String dbPw = dbIdPw[1];
-            if (id.equals(dbId)) {
-                if (pw.equals(dbPw)) {
-                    return "login";
-                } else {
-                    return "wrong pw";
-                }
+            map.put(dbIdPw[0], dbIdPw[1]);
+        }
+        
+        if (map.containsKey(id)) {
+            if (map.get(id).equals(pw)) {
+                return "login";
+            } else {
+                return "wrong pw";
             }
         }
         return "fail";
