16a3be6ba73a355019e0e6d23c7f3d0f3c4b6e21
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-11-12 17:20:16 +0900

    [level 0] Title: 특이한 정렬, Time: 9.60 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        return Arrays.stream(numlist)
+            .boxed()
+            .sorted((a, b) -> {
+                int distanceA = Math.abs(a - n);
+                int distanceB = Math.abs(b - n);
+                return distanceA == distanceB ? b - a : distanceA - distanceB;
+            })
+            .mapToInt(Integer::intValue)
+            .toArray();
+    }
+}
