e357ab9412ddc319497c09b6d5d575fce1123ec7
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-12-03 16:45:57 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.30 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,10 +2,7 @@ class Solution {
     public String solution(int[] food) {
         StringBuilder left = new StringBuilder();
         for (int i = 1; i < food.length; i++) {
-            int n = food[i] / 2;
-            for (int j = 0; j < n; j++) {
-                left.append(i);
-            }
+            left.append(Integer.toString(i).repeat(food[i] / 2));
         }
         StringBuilder right = new StringBuilder(left).reverse();
         return left.append(0).append(right).toString();
