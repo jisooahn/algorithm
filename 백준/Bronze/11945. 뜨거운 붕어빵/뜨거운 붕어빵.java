814485811c8326269028f3fe73e632e35494f673
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-12-09 16:34:59 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 96 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -1,20 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        if (m > 0) {
-            for (int i = 0; i < n; i++) {
-                char[] charArray = sc.next().toCharArray();
-                for (int j = m - 1; j >= 0; j--) {
-                    System.out.print(charArray[j]);
-                }
-                System.out.println();
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            for (int j = m - 1; j >= 0; j--) {
+                bw.write(input.charAt(j));
             }
+            bw.newLine();
         }
+
+        bw.flush();
     }
 }
