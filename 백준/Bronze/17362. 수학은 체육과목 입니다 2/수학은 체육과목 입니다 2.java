20a83383eb052490d734857d7a4da6dd580e6537
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-12-19 17:35:25 +0900

    [Bronze IV] Title: 수학은 체육과목 입니다 2, Time: 172 ms, Memory: 17624 KB -BaekjoonHub

--- 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.java
+++ 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.java
@@ -5,16 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if (n % 8 == 1) {
-            System.out.println(1);
-        } else if (n % 8 == 0 || n % 8 == 2) {
-            System.out.println(2);
-        } else if (n % 8 == 3 || n % 8 == 7) {
-            System.out.println(3);
-        } else if (n % 8 == 4 || n % 8 == 6) {
-            System.out.println(4);
-        } else {
-            System.out.println(5);
-        }
+        int[] fingers = {2, 1, 2, 3, 4, 5, 4, 3};
+        System.out.println(fingers[n % 8]);
     }
 }
