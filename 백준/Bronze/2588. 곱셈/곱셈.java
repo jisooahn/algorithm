25b6b353b3ab62f821398bb8f847a80ce578db4e
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-12-20 16:10:29 +0900

    [Bronze III] Title: 곱셈, Time: 172 ms, Memory: 17608 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -5,16 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int first = sc.nextInt();
-        char[] second = sc.next().toCharArray();
-        
-        int sum = 0;
-        int digit = 1;
-        for (int i = second.length - 1; i >= 0; i--) {
-            int tmp = Integer.parseInt(String.valueOf(second[i]));
-            System.out.println(first * tmp);
-            sum += (first * tmp) * digit;
-            digit *= 10;
-        }
-        System.out.println(sum);
+        int second = sc.nextInt();
+
+        System.out.println(first * (second % 10));
+        System.out.println(first * ((second / 10) % 10));
+        System.out.println(first * (second / 100));
+        System.out.println(first * second);
     }
 }
