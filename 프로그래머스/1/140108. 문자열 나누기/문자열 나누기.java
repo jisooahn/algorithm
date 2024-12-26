df9174d63f20e289ab967326b425f57cc7b6b261
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-12-26 17:55:24 +0900

    [level 1] Title: 문자열 나누기, Time: 0.82 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -2,27 +2,24 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         int index = 0;
-
+        
         while (index < s.length()) {
-            answer++;
-            
-            char c = s.charAt(index);
+            char x = s.charAt(index);
             int sameCount = 0;
             int diffCount = 0;
-
+            
             while (index < s.length()) {
-                if (s.charAt(index) == c) {
+                char c = s.charAt(index++);
+                if (x == c) {
                     sameCount++;
                 } else {
                     diffCount++;
                 }
-
-                index++;
-
                 if (sameCount == diffCount) {
                     break;
                 }
             }
+            answer++;
         }
         return answer;
     }
