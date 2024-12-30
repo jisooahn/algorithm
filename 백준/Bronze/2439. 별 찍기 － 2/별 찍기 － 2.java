62808fdb0107375238a0b4b43347d4b60db2be54
jisooahn <159110766+jisooahn@users.noreply.github.com>
2024-12-30 18:50:12 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 268 ms, Memory: 18436 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i = 1; i <= n; i++) {
-            for (int j = n; j > 0; j--) {
+            for (int j = n; j >= 1; j--) {
                 if (i >= j) {
                     System.out.print("*");
                 } else {
