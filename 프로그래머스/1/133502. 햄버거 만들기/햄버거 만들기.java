375d743ba5a8ce515708b3cf184570d6467e922f
jisooahn <159110766+jisooahn@users.noreply.github.com>
2025-01-03 21:26:05 +0900

    [level 1] Title: 햄버거 만들기, Time: 58.82 ms, Memory: 153 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -7,7 +7,7 @@ class Solution {
         for (int i : ingredient) {
             st.push(i);
             
-            if (st.size() >= 4) {
+            if (st.size() >= 4 && st.peek() == 1) {
                 if (st.get(st.size() - 4) == 1
                     && st.get(st.size() - 3) == 2
                     && st.get(st.size() - 2) == 3
