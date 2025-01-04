6dcf47627969b17a411c1111946c524ecb84c392
jisooahn <159110766+jisooahn@users.noreply.github.com>
2025-01-04 23:25:18 +0900

    [level 1] Title: 대충 만든 자판, Time: 4.32 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,26 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
         int[] answer = new int[targets.length];
         
+        Map<Character, Integer> map = new HashMap<>();
+        for (String key : keymap) {
+            for (int i = 0; i < key.length(); i++) {
+                char c = key.charAt(i);
+                map.put(c, Math.min(map.getOrDefault(c, Integer.MAX_VALUE), i + 1));
+            }
+        }
+        
         for (int i = 0; i < targets.length; i++) {
             int count = 0;
-            
-            for (int j = 0; j < targets[i].length(); j++) {
-                char c = targets[i].charAt(j);
-                int minK = Integer.MAX_VALUE;
-                
-                for (String key : keymap) {
-                    int k = key.indexOf(c);
-                    if (k != -1) {
-                        minK = Math.min(k, minK);
-                    }
-                }
-                
-                if (minK == Integer.MAX_VALUE) {
+            for (char c : targets[i].toCharArray()) {
+                if (!map.containsKey(c)) {
                     count = -1;
                     break;
                 }
-                count += minK + 1;
+                count += map.get(c);
             }
             answer[i] = count;
         }
