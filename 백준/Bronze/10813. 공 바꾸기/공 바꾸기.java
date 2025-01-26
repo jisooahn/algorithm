da8eb25bdff48bb945deaa13d41d7d8273cfa066
jisooahn <159110766+jisooahn@users.noreply.github.com>
2025-01-26 17:10:48 +0900

    [Bronze II] Title: 공 바꾸기, Time: 204 ms, Memory: 18500 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] basket = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            basket[i] = i;
+        }
+        for (int count = 0; count < m; count++) {
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+            int tmp = basket[i];
+            basket[i] = basket[j];
+            basket[j] = tmp;
+        }
+        for (int i = 1; i <= n; i++) {
+            System.out.print(basket[i] + " ");
+        }
+    }
+}
