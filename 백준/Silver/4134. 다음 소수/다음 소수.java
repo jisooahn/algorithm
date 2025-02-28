930042b9fa197f91c2437bba04fe5d1d23059883
jisooahn <159110766+jisooahn@users.noreply.github.com>
2025-02-28 17:50:53 +0900

    [Silver IV] Title: 다음 소수, Time: 472 ms, Memory: 22936 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            long tmp = sc.nextLong();
+            for (long j = tmp; ; j++) {
+                if (isPrime(j)) {
+                    System.out.println(j);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static boolean isPrime(long n) {
+        if (n < 2) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
+        for (long i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
