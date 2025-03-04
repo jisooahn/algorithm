cedaa10456e5b3c3c88ef46fdd1fe565722219ea
jisooahn <159110766+jisooahn@users.noreply.github.com>
2025-03-04 18:48:36 +0900

    [Silver III] Title: 소수 구하기, Time: 704 ms, Memory: 34664 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -9,15 +9,16 @@ public class Main {
         for (int i = 2; i <= n; i++) {
             isPrime[i] = true;
         }
-        
+
         for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= n; j += i) {
-                    isPrime[j] = false;
-                }
+            if (!isPrime[i]) {
+                continue;
+            }
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
             }
         }
-        
+
         for (int i = m; i <= n; i++) {
             if (isPrime[i]) {
                 System.out.println(i);
