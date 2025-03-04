5eddeec864ca72c27a4be54f0e300b4a2efa25bb
jisooahn <159110766+jisooahn@users.noreply.github.com>
2025-03-04 18:16:32 +0900

    [Silver III] Title: 소수 구하기, Time: 712 ms, Memory: 34776 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -5,22 +5,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
         int n = sc.nextInt();
-        int[] numbers = new int[n + 1];
+        boolean[] isPrime = new boolean[n + 1];
         for (int i = 2; i <= n; i++) {
-            numbers[i] = i;
+            isPrime[i] = true;
         }
         
         for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (numbers[i] == 0) {
-                continue;
-            }
-            for (int j = i * i; j <= n; j += i) {
-                numbers[j] = 0;
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
             }
         }
         
         for (int i = m; i <= n; i++) {
-            if (numbers[i] != 0) {
+            if (isPrime[i]) {
                 System.out.println(i);
             }
         }
