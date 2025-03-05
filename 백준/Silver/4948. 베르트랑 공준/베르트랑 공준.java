b7a7f4c8c13cd5f6ca112d20e0d71ccff982e82b
jisooahn <159110766+jisooahn@users.noreply.github.com>
2025-03-05 22:00:48 +0900

    [Silver II] Title: 베르트랑 공준, Time: 252 ms, Memory: 21032 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -3,6 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        final int LIMIT = 123456 * 2;
+        final boolean[] isPrime = new boolean[LIMIT + 1];
+        
+        for (int i = 2; i <= LIMIT; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i <= LIMIT; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= LIMIT; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
         while (true) {
             int n = sc.nextInt();
             if (n == 0) {
@@ -13,23 +28,11 @@ public class Main {
             int start = n + 1;
             int end = 2 * n;
             for (int i = start; i <= end; i++) {
-                if (isPrime(i)) {
+                if (isPrime[i]) {
                     count++;
                 }
             }
             System.out.println(count);
         }
-    }
-
-    public static boolean isPrime(int n) {
-        if (n < 2) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
