01dae415f82ab70d0c8808ab77450f4866f567e4
jisooahn <159110766+jisooahn@users.noreply.github.com>
2025-03-12 20:43:44 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 752 ms, Memory: 119420 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -9,15 +9,15 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[][] matrix = new int[n + 1][n + 1];
         int[][] sum = new int[n + 1][n + 1];
+        
         for (int row = 1; row <= n; row++) {
             st = new StringTokenizer(br.readLine());
             for (int col = 1; col <= n; col++) {
-                matrix[row][col] = Integer.parseInt(st.nextToken());
-                sum[row][col] = sum[row - 1][col] + sum[row][col - 1] + matrix[row][col] - sum[row - 1][col - 1];
+                sum[row][col] = sum[row - 1][col] + sum[row][col - 1] + Integer.parseInt(st.nextToken()) - sum[row - 1][col - 1];
             }
         }
+        
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
