57e4bc191c17c83bf01edf829e2567e5b686aa8d
jisooahn <159110766+jisooahn@users.noreply.github.com>
2025-03-14 15:02:51 +0900

    [Silver IV] Title: 주몽, Time: 560 ms, Memory: 34204 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] + array[j] == m) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
